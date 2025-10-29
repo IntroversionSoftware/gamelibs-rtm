fbe4e9a9a966523f0d2c8d5ff6c1064317b54946
Steven Noonan <steven@uplinklabs.net>
2025-10-28 23:20:52 -0700

    split implementation of functions using vector_mix to reduce compile time
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

---
+++ includes/rtm/impl.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "matrix3x3d.h"
+#include "matrix3x3d_impl.h"
+#include "matrix3x3f.h"
+#include "matrix3x3f_impl.h"
+#include "matrix3x4d.h"
+#include "matrix3x4d_impl.h"
+#include "matrix3x4f.h"
+#include "matrix3x4f_impl.h"
+#include "matrix4x4d.h"
+#include "matrix4x4d_impl.h"
+#include "matrix4x4f.h"
+#include "matrix4x4f_impl.h"
+#include "vector4d.h"
+#include "vector4d_impl.h"
+
